f1f381f2e9c68f8931fe49e70aee07d97049d5be
Koushik <koushik984@gmail.com>
2017-02-09 10:25:37 +0100

    Added solutons for Azure Functon and AppInsights

--- LetsTryAppInsights/src/LetsTryAppInsights/Controllers/ValuesController.cs
+++ LetsTryAppInsights/src/LetsTryAppInsights/Controllers/ValuesController.cs
@@ -2,22 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LetsTryAppInsights.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        public TelemetryClient _telemetry = new TelemetryClient(TelemetryConfiguration.Active);
 
-        public TelemetryClient _telemetry = new TelemetryClient();
-
+        public ValuesController()
+        {
+            _telemetry.InstrumentationKey = "afd1ae9b-0d73-4ceb-b25f-9a9c0a00dc60";
+        }
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
             _telemetry.TrackEvent($"Now into : {HttpContext.Request.Path}");
+
             return new string[] { "value1", "value2" };
         }
 
@@ -25,7 +30,15 @@ namespace LetsTryAppInsights.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            _telemetry.TrackEvent($"Now into : {HttpContext.Request.Path}");
+            if (id == 1)
+                return "value";
+            else
+            {
+                var e = new Exception("some stacktrace");
+                _telemetry.TrackException(e, new Dictionary<string, string>() {{"Id", id.ToString()}});
+                throw e;
+            }
         }
 
         // POST api/values
