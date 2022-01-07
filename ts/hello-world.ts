8ba9f4a64e59be7d63e8c3ad783f6d50785bab45
Koushik <koushik984@gmail.com>
2022-01-07 14:39:33 +0100

    typescript-async/await and class

---
+++ ts/hello-world.ts
@@ -0,0 +1,37 @@
+import { myMath } from "./myMath";
+
+//async function getSum(params: Number[]) {
+const getSum = async(params: Number[]) => {
+    var total: number = 0;
+    params.forEach(element => {
+        total += element.valueOf()
+    });
+    return new Promise(resolve => resolve({ message: "elements: "+params + ", total: " + total }))
+    // console.log("elements: " + params)
+    // console.log("total: " + total)
+}
+
+// do sum of number in a list
+let intlist = new Array();
+for (var i = 0; i < 3; i++) {
+    intlist.push(i)
+}
+
+// call async function and console log 
+(async () => {
+    console.log(await getSum(intlist));
+  }
+)();
+
+// create list with random numbers between a ceiling and floor
+// var r = new myMath(5, 100, 50).generateRandomNumberSeries()
+// console.log(r);
+
+// return fibanocci series of size
+var r = new myMath(15, 100, 50).getFibanocciSeries()
+console.log(r);
+
+function timeout(arg0: number) {
+    return new Promise(i => setTimeout(i, arg0))
+}
+
---
+++ ts/myMath.ts
@@ -0,0 +1,42 @@
+export class myMath {
+    private _size: number = 0
+    private _max: number = 0
+    private _min: number = 0
+    
+    constructor(size: number, min: number, max: number) {
+        this._size = size
+        this._max = max 
+        this._min = min
+    }
+
+    private generateRandomNumber(): number {
+        return Math.floor(Math.random() * (Math.ceil(this._max) - Math.floor(this._min)) + 1) + Math.floor(this._min)
+    }
+
+    generateRandomNumberSeries(): Array<number> {
+        var randomList = new Array();
+        var listSize:number = 0;
+
+        while (listSize < this._size) {
+            randomList.push(this.generateRandomNumber())
+            listSize++
+        }
+        return randomList;
+    }
+
+    // 0,1,1,2,3,5,8...
+    getFibanocciSeries() : Array<number>{
+        var fib = new Array();
+        var idx = 0;
+        while(idx < this._size){
+            fib.push(this.getFibnumber(idx))
+            idx++
+        }
+        return fib;
+    }
+
+    getFibnumber(atPosition:number): number{
+        return atPosition < 2 ? 1: this.getFibnumber(atPosition - 1 ) + this.getFibnumber(atPosition - 2)
+    }
+
+}
